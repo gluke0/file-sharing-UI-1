16fc2e384ae695977b0477f2d50e050f1c1de3e3
gluke0 <milanesigianluca@gmail.com>
2025-05-20 14:13:22 +0200

    left section

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 let sidebarItems = document.querySelectorAll('.sidebar .item');
 let tableRows = document.querySelectorAll('.main-section table tbody tr');
 let menuButton = document.querySelectorAll('.menu-btn');
+let leftSection = document.querySelectorAll('.left-section');
