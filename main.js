4a8bdb95191df56e68b230f9da30a0cea9278506
gluke0 <milanesigianluca@gmail.com>
2025-05-20 14:17:01 +0200

    active item click

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ let sidebarItems = document.querySelectorAll('.sidebar .item');
 let tableRows = document.querySelectorAll('.main-section table tbody tr');
 let menuButton = document.querySelectorAll('.menu-btn');
 let leftSection = document.querySelectorAll('.left-section');
+
+let openMenu = false;
+
+sidebarItems.forEach(sideItem =>{
+    sideItem.addEventListener('click', () =>{
+        sidebarItems.forEach(item =>{
+            item.classList.remove('active');
+        });
+        sideItem.classList.add('active');
+    });
+});
